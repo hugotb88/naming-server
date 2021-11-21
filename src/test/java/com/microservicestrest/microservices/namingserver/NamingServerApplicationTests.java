3fcaab15f21457561a682b79b33bee71b24617da
Victor Hugo Olvera Cruz <hugotb88@gmail.com>
2021-11-21 13:43:52 -0600

    First Commit

---
+++ src/test/java/com/microservicestrest/microservices/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicestrest.microservices.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
